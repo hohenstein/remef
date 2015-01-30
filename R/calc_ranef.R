c18e5bdb2eca3ce7add54908a5ea86cf25932e25
hohenstein <sven.hohenstein@gmx.de>
2015-01-30 22:07:35 +0100

    removed trailing spaces

--- R/calc_ranef.R
+++ R/calc_ranef.R
@@ -37,14 +37,14 @@ function(ran, model)
   }
   ran_labs <- ranef_labels(model)
   rf_labs <- names(ran_labs)
-  if ( is.null(names(ran)) || 
+  if ( is.null(names(ran)) ||
          any( !names(ran) %in% rf_labs ) ||
-         any(duplicated(names(ran))) 
+         any(duplicated(names(ran)))
   )
     stop("The list 'ran' has invalid names.")
   
   # number of random effects for each random factor
-  num_re <- vapply(ran_labs, length, FUN.VALUE = integer(1L), 
+  num_re <- vapply(ran_labs, length, FUN.VALUE = integer(1L),
                    USE.NAMES = FALSE)
   num_re_previous <- c(0L, cumsum(head(num_re, -1L)))
   
@@ -52,29 +52,29 @@ function(ran, model)
   ran_model <- ranef(model)
   
   # subset of random effects corresponding to 'ran'
-  ran_model_sub <- lapply(mapply("[", ran_model[rf_idx], ran, 
+  ran_model_sub <- lapply(mapply("[", ran_model[rf_idx], ran,
                                  SIMPLIFY = FALSE),
-                          unlist)    
+                          unlist)
   
   # transform the list of 'Zt' matrices into a list of lists
-  re_list_idx <- mapply(function(x, y) seq_len(x) + y, 
+  re_list_idx <- mapply(function(x, y) seq_len(x) + y,
                         num_re, num_re_previous,
-                        SIMPLIFY = FALSE)  
-  re_list_flat <- getME(model, "Ztlist")  
+                        SIMPLIFY = FALSE)
+  re_list_flat <- getME(model, "Ztlist")
   re_list <- lapply(re_list_idx, function(x) re_list_flat[x])
   
   # extract subsets
   re_list_sub <- mapply("[", re_list[rf_idx], ran,
-                        SIMPLIFY = FALSE)  
-  ran_model_sub <- mapply("[", ran_model[rf_idx], ran, 
+                        SIMPLIFY = FALSE)
+  ran_model_sub <- mapply("[", ran_model[rf_idx], ran,
                           SIMPLIFY = FALSE)
   
   # here the calculation takes place
-  ranef_prods <- mapply(function(mats, vecs) 
-    mapply(function(v, m) as.vector(v %*% m), 
+  ranef_prods <- mapply(function(mats, vecs)
+    mapply(function(v, m) as.vector(v %*% m),
            vecs, mats),
     re_list_sub, ran_model_sub,
-    SIMPLIFY = FALSE)  
-  ranef_sums <- rowSums(do.call(cbind, ranef_prods))  
-  ranef_sums  
+    SIMPLIFY = FALSE)
+  ranef_sums <- rowSums(do.call(cbind, ranef_prods))
+  ranef_sums
 }
